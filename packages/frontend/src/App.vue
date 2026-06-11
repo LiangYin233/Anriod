894622809ea85c8379fc2a975bd74e3b53c5bb32
LiangYin233 <liangyin233@users.noreply.github.com>
2026-06-11 11:58:16 +0800

    fix: always show scrollbar to prevent layout shift on page switch

--- packages/frontend/src/App.vue
+++ packages/frontend/src/App.vue
@@ -236,7 +236,7 @@ const mainPadding = computed(() => sidebarCollapsed.value ? 'lg:pl-16' : 'lg:pl-
       </header>
 
       <!-- Page Content -->
-      <main class="relative flex-1 overflow-y-auto">
+      <main class="relative flex-1 overflow-y-scroll">
         <div class="px-container-padding py-6 lg:px-8">
           <RouterView />
         </div>
